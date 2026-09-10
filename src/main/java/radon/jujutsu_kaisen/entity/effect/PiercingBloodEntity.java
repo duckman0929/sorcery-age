418a86cb6c6e8c774e7e55bf2407a72eceaf6cc3
duckman0929 <1704499@fcpsschools.net>
2026-09-10 10:25:04 -0400

    Create PiercingBloodEntity.java

---
+++ src/main/java/radon/jujutsu_kaisen/entity/effect/PiercingBloodEntity.java
@@ -0,0 +1,67 @@
+package radon.jujutsu_kaisen.entity.effect;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.damagesource.DamageSource;
+import radon.jujutsu_kaisen.damage.JJKDamageSources;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+import radon.jujutsu_kaisen.ability.JJKAbilities;
+import radon.jujutsu_kaisen.ability.base.Ability;
+import radon.jujutsu_kaisen.entity.JJKEntities;
+import radon.jujutsu_kaisen.entity.effect.base.BeamEntity;
+import radon.jujutsu_kaisen.util.RotationUtil;
+
+public class PiercingBloodEntity extends BeamEntity {
+    public PiercingBloodEntity(EntityType<? extends Projectile> pType, Level pLevel) {
+        super(pType, pLevel);
+    }
+
+    public PiercingBloodEntity(LivingEntity owner, float power) {
+        super(JJKEntities.PIERCING_BLOOD.get(), owner, power);
+    }
+
+    @Override
+    public int getFrames() {
+        return 12;
+    }
+
+    @Override
+    public float getScale() {
+        return 0.5F;
+    }
+
+    @Override
+    protected double getRange() {
+        return 32.0D;
+    }
+
+    @Override
+    protected float getDamage() {
+        return 11.0F;
+    }
+
+    @Override
+    public int getDuration() {
+        return 12;
+    }
+
+    @Override
+    public int getCharge() {
+        return 0;
+    }
+
+
+    @Override
+    protected @Nullable Ability getSource() {
+        return JJKAbilities.PIERCING_BLOOD.get();
+    }
+
+    @Override
+    protected Vec3 calculateSpawnPos(LivingEntity owner) {
+        return new Vec3(owner.getX(), owner.getY() + (owner.getBbHeight() * 0.75F) - (this.getBbHeight() / 2.0F), owner.getZ())
+                .add(RotationUtil.getTargetAdjustedLookAngle(owner));
+    }
+}
